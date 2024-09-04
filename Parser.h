beca62564bc68782f29594511f981dbe0f985b37
Tom Billingsley <tom.billingsley@gmail.com>
2024-09-04 11:04:14 -0400

    Rearrange; put operations up front

--- Parser.h
+++ Parser.h
@@ -8,43 +8,11 @@
 using namespace std;
 
 class AParser {
-public:
-  class APair {
-  public:
-    string fKey;
-    string fValue;
-  };
-
-  class ASection : public vector<APair> {
-  public:
-    string name;
-  };
-
-  class ASectionVector : public vector<ASection> {
-  public:
-    void Store(ASection *section);
-  };
-
-  ASectionVector &Sections() { return fSections; }
-
-private:
-  void Save();
-
-  const int handleNonSectionName(int linenum, string &buf);
-  const int handleSectionName(string &);
-  void handlePossibleContinuationLine(string &buf);
-  void storePendingSection();
-  const bool alreadySeen(string &) const;
-  void clearAllSection();
-
-  vector<APair> tempPairs;
-  string prevSectionName;
 
 public:
+
+  // Create a parser object given a file name.
   AParser(string s);
-  ~AParser();
-
-  const int Parse();
 
   const bool HasSection(const string &) const;
   const int NumSections() const { return fSections.size(); }
@@ -62,9 +30,32 @@ public:
   void SetFloat(const string &section, const string &key, const float value);
 
 private:
+
   void GetLine(ifstream &, string &) const;
   void Erase();
 
+public:
+  const int Parse();
+    ~AParser();
+  class APair {
+  public:
+    string fKey;
+    string fValue;
+  };
+
+  class ASection : public vector<APair> {
+  public:
+    string name;
+  };
+
+  class ASectionVector : public vector<ASection> {
+  public:
+    void Store(ASection *section);
+  };
+
+  ASectionVector &Sections() { return fSections; }
+  vector<APair> tempPairs;
+  
 public:
   enum kErrors {
     kErrorSuccess = 0,
@@ -73,6 +64,19 @@ public:
     kErrorDuplicateKey,
     kErrorDoesntStartWithSection
   };
+
+private:
+  void Save();
+
+  const int handleNonSectionName(int linenum, string &buf);
+  const int handleSectionName(string &);
+  void handlePossibleContinuationLine(string &buf);
+  void storePendingSection();
+  const bool alreadySeen(string &) const;
+  void clearAllSection();
+
+
+  string prevSectionName;
 
 private:
   ASectionVector fSections;
